1f67e7926ad46d55cb43cd2afa5bbfeb293b2ff6
mavrara <yevheniia.besedina@outlook.com>
2023-04-25 12:22:51 +0300

    lab 4.2

---
+++ Prime Numbers/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Prime_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run = new System.Windows.Forms.Button();
+            this.maxNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.results = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.run.Location = new System.Drawing.Point(16, 63);
+            this.run.Margin = new System.Windows.Forms.Padding(4);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(749, 28);
+            this.run.TabIndex = 0;
+            this.run.Text = "Сформувати прості числа";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // maxNumber
+            // 
+            this.maxNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maxNumber.Location = new System.Drawing.Point(398, 31);
+            this.maxNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.maxNumber.Name = "maxNumber";
+            this.maxNumber.Size = new System.Drawing.Size(366, 22);
+            this.maxNumber.TabIndex = 1;
+            this.maxNumber.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(395, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Максимальне число діапазону:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progress
+            // 
+            this.progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progress.Location = new System.Drawing.Point(16, 98);
+            this.progress.Margin = new System.Windows.Forms.Padding(4);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(749, 28);
+            this.progress.Step = 1;
+            this.progress.TabIndex = 3;
+            // 
+            // results
+            // 
+            this.results.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.results.Location = new System.Drawing.Point(17, 137);
+            this.results.Margin = new System.Windows.Forms.Padding(4);
+            this.results.Multiline = true;
+            this.results.Name = "results";
+            this.results.ReadOnly = true;
+            this.results.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.results.Size = new System.Drawing.Size(748, 246);
+            this.results.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Мінімальне число діапазону:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(16, 31);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 396);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxNumber);
+            this.Controls.Add(this.run);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(797, 433);
+            this.Name = "Form1";
+            this.Text = "Програма генератора великих простих чисел ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.TextBox maxNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Prime Numbers/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+
+namespace Prime_Numbers
+{
+    public partial class Form1 : Form
+    {
+        bool working = false;
+        int minN = 0;
+        int maxN = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n > 1)
+            {
+                return Enumerable.Range(1, n).Where(x => n % x == 0).SequenceEqual(new[] { 1, n });
+            }
+
+            return false;
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            if (!working) {
+                try
+                {
+                    working = true;
+                    maxN = int.Parse(maxNumber.Text);
+                    minN = int.Parse(textBox1.Text);
+                    results.Text = "";
+                    progress.Maximum = maxN;
+                    for (int i = minN; i <= maxN; i++)
+                    {
+                        Form1.ActiveForm.Update();
+                        progress.Value = i;
+                        if (IsPrime(i))
+                        {
+                            Console.WriteLine(i + " Просте число");
+                            results.Text += i + "\r\n";
+                            results.Update();
+                        }
+                        else
+                        {
+                            Console.WriteLine(i + " Не просте число");
+                        }
+                    }
+                    working = false;
+                }
+                catch (Exception ex)
+                {
+                    progress.Value = 0;
+                    working = false;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
